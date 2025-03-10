a6b621955ba443c929661e7519c8c08cfee26585
Robsu66 <robsondiaswp@gmail.com>
2025-03-10 19:49:31 -0300

    LWC Accordion

--- force-app/main/default/lwc/showOpportunity/showOpportunity.js
+++ force-app/main/default/lwc/showOpportunity/showOpportunity.js
@@ -1,25 +1,54 @@
-import { LightningElement } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { LightningElement, api, wire, track } from 'lwc';
+import getOpportunities from '@salesforce/apex/showOpportunityController.getOpportunities';
+import getProducts from '@salesforce/apex/showOpportunityController.getProducts';
 
 export default class OpportunityForm extends LightningElement {
     
     spinner = false;
+    allData;
+    allProducts = [];
+    productsMap = new Map();
 
-    handleSubmit(event) {
-        this.spinner = true;
-        event.preventDefault();
-        const fields = event.detail.fields;
-        this.template.querySelector('lightning-record-edit-form').submit(fields);
+    @wire(getOpportunities)
+    wireData({error, data}) {
+        if (data) {
+            this.allData = data;
+            let opportunityIds = [];
+            
+            Object.values(this.allData).forEach(opportunity => {
+                opportunityIds.push(opportunity.Id);
+            });
+
+            this.fetchProducts(opportunityIds);
+        } else if (error) {
+            console.error("Error fetching opportunities:", error);
+        }
     }
 
-    handleSuccess(event) {
-        const toastEvent = new ShowToastEvent({
-            title: 'Sucesso',
-            message: 'Produto adicionado com sucesso',
-            variant: 'success',
-        });
-        this.dispatchEvent(toastEvent);
-        this.spinner = false;
-        
+    async fetchProducts(opportunityIds) {
+        try {
+            this.allProducts = await getProducts({ opportunityIds: opportunityIds });
+            console.log("Products:", this.allProducts);
+            console.log(opportunityIds);
+
+            this.productsMap.clear();
+
+            for (let i = 0; i < this.allProducts.length; i++) {
+                let opportunityId = this.allProducts[i].Opportunity__c;
+                let product = this.allProducts[i];
+
+                if (opportunityId) {
+                    if (!this.productsMap.has(opportunityId)) {
+                        this.productsMap.set(opportunityId, []);
+                    }
+                    this.productsMap.get(opportunityId).push(product);
+                }
+            }
+
+            console.log("Products grouped by Opportunity:", this.productsMap);
+
+        } catch (error) {
+            console.error("Error fetching products:", error);
+        }
     }
 }
