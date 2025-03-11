6b49268b7785ae526e8352fec65e41314c83adf2
Robsu66 <robsondiaswp@gmail.com>
2025-03-11 10:08:27 -0300

    Accordion mostrando produtos associados à oportunidade

--- force-app/main/default/lwc/showOpportunity/showOpportunity.js
+++ force-app/main/default/lwc/showOpportunity/showOpportunity.js
@@ -1,54 +1,62 @@
-import { LightningElement, api, wire, track } from 'lwc';
-import getOpportunities from '@salesforce/apex/showOpportunityController.getOpportunities';
-import getProducts from '@salesforce/apex/showOpportunityController.getProducts';
+import { LightningElement, wire, track } from "lwc"
+import getOpportunities from "@salesforce/apex/showOpportunityController.getOpportunities"
+import getProducts from "@salesforce/apex/showOpportunityController.getProducts"
 
 export default class OpportunityForm extends LightningElement {
-    
-    spinner = false;
-    allData;
-    allProducts = [];
-    productsMap = new Map();
+  spinner = false
+  @track allData = []
+  @track opportunitiesWithProducts = []
+  allProducts = []
+  productsMap = new Map()
 
-    @wire(getOpportunities)
-    wireData({error, data}) {
-        if (data) {
-            this.allData = data;
-            let opportunityIds = [];
-            
-            Object.values(this.allData).forEach(opportunity => {
-                opportunityIds.push(opportunity.Id);
-            });
+  @wire(getOpportunities)
+  wireData({ error, data }) {
+    if (data) {
+      this.allData = data
+      const opportunityIds = []
 
-            this.fetchProducts(opportunityIds);
-        } else if (error) {
-            console.error("Error fetching opportunities:", error);
-        }
+      Object.values(this.allData).forEach((opportunity) => {
+        opportunityIds.push(opportunity.Id)
+      })
+
+      this.fetchProducts(opportunityIds)
+    } else if (error) {
+      console.error("Error fetching opportunities:", error)
     }
+  }
 
-    async fetchProducts(opportunityIds) {
-        try {
-            this.allProducts = await getProducts({ opportunityIds: opportunityIds });
-            console.log("Products:", this.allProducts);
-            console.log(opportunityIds);
+  async fetchProducts(opportunityIds) {
+    try {
+      this.allProducts = await getProducts({ opportunityIds: opportunityIds })
+      console.log(opportunityIds)
 
-            this.productsMap.clear();
+      this.productsMap.clear()
 
-            for (let i = 0; i < this.allProducts.length; i++) {
-                let opportunityId = this.allProducts[i].Opportunity__c;
-                let product = this.allProducts[i];
+      for (let i = 0; i < this.allProducts.length; i++) {
+        const opportunityId = this.allProducts[i].Opportunity__c
+        const product = this.allProducts[i]
 
-                if (opportunityId) {
-                    if (!this.productsMap.has(opportunityId)) {
-                        this.productsMap.set(opportunityId, []);
-                    }
-                    this.productsMap.get(opportunityId).push(product);
-                }
-            }
-
-            console.log("Products grouped by Opportunity:", this.productsMap);
-
-        } catch (error) {
-            console.error("Error fetching products:", error);
+        if (opportunityId) {
+          if (!this.productsMap.has(opportunityId)) {
+            this.productsMap.set(opportunityId, [])
+          }
+          this.productsMap.get(opportunityId).push(product)
         }
+      }
+
+      // Create a new array with opportunities and their products
+      this.opportunitiesWithProducts = this.allData.map((opp) => {
+        return {
+          ...opp,
+          products: this.productsMap.get(opp.Id) || [],
+        }
+      })
+
+      const arrayOfEntries = Array.from(this.productsMap.entries())
+      console.log("" + arrayOfEntries)
+    } catch (error) {
+      console.error("Error fetching products:", error)
     }
+  }
 }
+
